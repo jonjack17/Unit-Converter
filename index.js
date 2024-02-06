b414fdfdf9daceffa492e1f395a20350ee9991a6
jonjack17 <jonjack17@gmail.com>
2024-02-05 21:14:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,24 @@ let userInput = document.getElementById("unit-input")
 
 
 
-convertBtn.addEventListener("click", function() {
-    let inputNumber=Number(userInput.value)
-    console.log(inputNumber*3)
-    //lengthResult.innerHTML = `${userInputNumber)} meters `
-})
+convertBtn.addEventListener("click", length) 
 
-//bababa
+function length() {
+    let inputNumber=Number(userInput.value)
+    let inputNumberDec=inputNumber.toFixed(2)
+    let inputNumberDecNumber=Number(inputNumberDec)
+
+    console.log(inputNumberDec)
+    lengthResult.innerHTML = `${inputNumberDec} meters = ${inputNumberDec*3.281} feet`
+}
+
+
+
+
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+//setInterval(function() {document.body.style.backgroundColor = "rgba(" + (256 * Math.random()) + "," + (256 * Math.random()) + "," + (256 * Math.random()) + "," + Math.random() + ")"}, 200)
